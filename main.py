4756a372c3e5403ffa69426bfae75a8542be22f1
Erich Hafenmaier <ehafenmaier@gmail.com>
2025-11-02 11:54:10 -0800

    Add functions to sort the characters into a list of dictionaries with their counts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_character_counts
 
 def load_book_text(file_path):
     with open(file_path) as file:
@@ -9,14 +9,16 @@ def main():
     book_text = load_book_text(book_file_path)
     word_count = count_words(book_text)
     character_counts = count_characters(book_text)
+    sorted_character_counts = sort_character_counts(character_counts)
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_file_path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("-------- Character Count --------")
-    for char in character_counts:
-        print(f"'{char}': {character_counts[char]}")
+    for char_count in sorted_character_counts:
+        if char_count["char"].isalpha():
+            print(f"{char_count["char"]}: {char_count["num"]}")
 
 
 
--- stats.py
+++ stats.py
@@ -4,11 +4,23 @@ def count_words(text):
     return len(words)
 
 def count_characters(text):
-    characters = {}
+    character_counts = {}
     for char in text:
         char = char.lower()
-        if char in characters:
-            characters[char] += 1
+        if char in character_counts:
+            character_counts[char] += 1
         else:
-            characters[char] = 1
-    return characters
+            character_counts[char] = 1
+    return character_counts
+
+def sort_on(items):
+    return items["num"]
+
+def sort_character_counts(character_counts):
+    sorted_characters = []
+    for char in character_counts:
+        sorted_char = {"char": char, "num": character_counts[char]}
+        sorted_characters.append(sorted_char)
+
+    sorted_characters.sort(reverse=True, key=sort_on)
+    return sorted_characters
