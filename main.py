0d4da471ecb07376c8f1a7772453c28a052a56fd
Erich Hafenmaier <ehafenmaier@gmail.com>
2025-11-02 10:36:27 -0800

    Create function to load text from a book and display in the console

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def load_book_text(file_path):
+    with open(file_path) as file:
+        return file.read()
+    
+def main():
+    book_text = load_book_text("./books/frankenstein.txt")
+    print(book_text)
+
+
+main()
